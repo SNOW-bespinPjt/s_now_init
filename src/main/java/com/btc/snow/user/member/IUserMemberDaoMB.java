b4f1760ff9f1a731fad1221d86d6b6c912a4e351
hansung1461 <hansung1461@gmail.com>
2023-08-31 10:34:55 +0900

    Refactor 코드 컨밴션 및 변수명 수정 java - user/member/UserMemberDto resources - mybatis/mappers/userMember-mapper.xml DB 컬럼명 변경에 따른 변수명 수정

--- src/main/java/com/btc/snow/user/member/IUserMemberDaoMB.java
+++ src/main/java/com/btc/snow/user/member/IUserMemberDaoMB.java
@@ -5,25 +5,39 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface IUserMemberDaoMB {
 
-	// CHECK DUPLICATE ID
-	public boolean isUser(String m_id);
+    /*
+     * CHECK DUPLICATE ID
+     */
+    public boolean isUser(String m_id);
 
-	// INSERT NEW ACCOUNT
-	public int insertUserMember(UserMemberDto memberDto);
+    /*
+     * INSERT NEW ACCOUNT
+     */
+    public int insertUserMember(UserMemberDto memberDto);
 
-	// USER LOGIN CONFIRM
-	public UserMemberDto selectUserForLogin(UserMemberDto userMemberDto);
+    /*
+     * USER LOGIN CONFIRM
+     */
+    public UserMemberDto selectUserForLogin(UserMemberDto userMemberDto);
 
-	// USER MODIFY CONFIRM
-	public int updateAccount(UserMemberDto userMemberDto);
+    /*
+     * USER MODIFY CONFIRM
+     */
+    public int updateAccount(UserMemberDto userMemberDto);
 
-	// GET USER INFO
-	public UserMemberDto getLatestAccountInfo(UserMemberDto userMemberDto);
+    /*
+     * GET USER Latest INFO
+     */
+    public UserMemberDto getLatestAccountInfo(UserMemberDto userMemberDto);
 
-	// USER DELETE CONFIRM
-	public int deleteUser(int u_m_no);
+    /*
+     * USER DELETE CONFIRM
+     */
+    public int deleteUser(int u_m_no);
 
-	// UPDATE USER PW
-	public int updateUserPW(UserMemberDto userMemberDto);
+    /*
+     * UPDATE USER PW
+     */
+    public int updateUserPW(UserMemberDto userMemberDto);
 
 }
