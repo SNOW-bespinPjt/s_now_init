19a7b123e15f1c74fac27d28f7c89e5b3860a598
iolagvi28 <iolagvi28@gmail.com>
2023-09-03 23:30:21 +0900

    Feat: AdminAssignmentController.java 초안 완료

--- src/main/java/com/btc/snow/user/assignment/IUserAssignmentService.java
+++ src/main/java/com/btc/snow/user/assignment/IUserAssignmentService.java
@@ -1,5 +1,21 @@
 package com.btc.snow.user.assignment;
 
+import java.util.List;
+
 public interface IUserAssignmentService {
 
+    // 과제 리스트(과제 페이지)
+    List<UserAssignmentDto> listAssignment();
+
+    // 과제 등록
+    int RegistrationConfirm(UserAssignmentDto userAssignmentDto);
+
+    // 과제 상세페이지
+    UserAssignmentDto getAssignment(int no);
+
+    // 과제 수정
+    int modifyAssignmentConfirm(UserAssignmentDto userAssignmentDto);
+
+    // 과제 삭제
+    int deleteAssignmentConfirm(int no);
 }
