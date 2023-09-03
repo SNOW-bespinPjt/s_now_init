46206dd2e20f2ee19d636d04bd68aeb2d6dfe1ab
iolagvi28 <iolagvi28@gmail.com>
2023-09-04 00:10:44 +0900

    Fix: AdminAssignmentDto, UserAssignmentDto 컬럼 추가(int is_submit)

--- src/main/java/com/btc/snow/admin/assignment/AdminAssignmentDto.java
+++ src/main/java/com/btc/snow/admin/assignment/AdminAssignmentDto.java
@@ -14,6 +14,7 @@ public class AdminAssignmentDto {
     private String file_name; //'과제 파일 사용자 지정 이름',
     private int point; //default 0  '과제점수 점수',
     private int is_activation; //default 0  '과제 활성화(비활성:0, 활성:1)',
+    private int is_submit; //default 0 '과제 제출(미제출:0, 제출:1)',
     private String start_date; //'과제 시작일',
     private String end_date; //'과제 종료일',
     private String reg_date; //'과제 등록일',
