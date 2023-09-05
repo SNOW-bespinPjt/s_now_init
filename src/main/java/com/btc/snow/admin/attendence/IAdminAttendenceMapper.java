a2bc1108814f94a80f3f945a2e9376ab99c4b357
yang <ysotgood@gmail.com>
2023-09-05 16:27:00 +0900

    feat 어드민 사이드 출결관리 view 페이지 네이션 적용

---
+++ src/main/java/com/btc/snow/admin/attendence/IAdminAttendenceMapper.java
@@ -0,0 +1,15 @@
+package com.btc.snow.admin.attendence;
+
+
+import com.btc.snow.include.SubmitDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface IAdminAttendenceMapper {
+    List<SubmitDto> selectSubmitInfo(Map<String, Object> map);
+
+    int getTotalCntOfSubmit();
+}
---
+++ src/main/java/com/btc/snow/admin/attendence/IAdminAttendenceService.java
@@ -0,0 +1,8 @@
+package com.btc.snow.admin.attendence;
+
+import java.util.Map;
+
+public interface IAdminAttendenceService {
+
+    Map<String, Object> selectSubmitDto(int no, int pageNum, int amount);
+}
