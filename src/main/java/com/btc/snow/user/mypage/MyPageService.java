ea010d8b22b1d0a10fc723b591f14065db59b96b
yang <ysotgood@gmail.com>
2023-09-06 18:02:18 +0900

    feat 마이페이지 스케쥴 view 생성 및 신청된 테이블 정보 조회

--- src/main/java/com/btc/snow/user/mypage/MyPageService.java
+++ src/main/java/com/btc/snow/user/mypage/MyPageService.java
@@ -1,10 +1,20 @@
 package com.btc.snow.user.mypage;
 
+import com.btc.snow.include.StudyPromiseDto;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
 public class MyPageService implements IMyPageService {
-    
+    @Autowired
+    IMyapgeMapper iMyapgeMapper;
+
+    @Override
+    public StudyPromiseDto selectScedule(String id) {
+        log.info("selectScedule(String id)");
+
+        return iMyapgeMapper.selectSceduleByUser(id);
+    }
 }
